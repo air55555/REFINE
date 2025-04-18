9334ab0ef07c8f372c369bf49840ff4bdcf4dc9c
air55555 <air55555@gmail.com>
2025-04-18 09:09:33 +0300

    fix dir struct

--- cli/sisr.bat
+++ cli/sisr.bat
@@ -1,6 +1,8 @@
+REM this could be run OUTSIDE the pycharm
 print %PYTHONPATH%
 SET PYTHONPATH=%PYTHONPATH%;D:\PycharmProjects\DPHSIRmy;D:\PycharmProjects\DPHSIRmy\etc
 print %PYTHONPATH%
-SET CUDA_VISIBLE_DEVICES = 0
+REM SET CUDA_VISIBLE_DEVICES = 2
 
-S:\Users\LRS\miniconda3\envs\hirdiff\python.exe D:/PycharmProjects/DPHSIRmy/cli/main.py -o timestamp -i D:\PycharmProjects\DPHSIRmy\input\2024-09-13_08-04-13_white_circ_t_(256,256,50)_cropped.mat -sf 8   sisr -it 4
+S:\Users\LRS\miniconda3\envs\hirdiff\python.exe D:/PycharmProjects/DPHSIRmy/cli/main.py -t no_gt --device cuda -o timestamp -i d:\PycharmProjects\DPHSIRmy\input\synergy(256,256,88).mat -sf 2 -d grunet -s admm sisr -it 100
+rem -o timestamp -i D:\PycharmProjects\DPHSIRmy\input\2024-09-13_08-04-13_white_circ_t_(256,256,50)_cropped.mat -sf 8   sisr -it 4
